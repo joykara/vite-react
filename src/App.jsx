7e0659649d2a4f428de4f7c165c05a2c318323d5
joykara <joykarani1@gmail.com>
2023-10-20 11:34:07 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
-import { useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// 3c5c786f
-const API_KEY = " http://www.omdbapi.com?apikey=3c5c786f "
+import { useState } from 'react'
 
 function App() {
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_KEY}&s=${title}`);
-    const data = await response.json();
-
-    console.log(data.Search);
-  }
-
-  useEffect(() => {
-    searchMovies('Minion')
-  }, [])
+  const [todo, setTodo] = useState();
 
   return (
-    <>
-      <div className='flex p-4 '>
-        <nav className=''>TODO</nav>
-        <section className="grocery-items">
-          <div className="searchBar">
-            <input type="text" />
+    <div className="flex h-screen w-full bg-violet-100 bg-opacity-95">
+      <div className="flex-col justify-center w-full mx-auto">
+        <h1 className='text-center font-serif p-5'>TODO LIST</h1>
+        <ul className="flex justify-evenly py-2 border-b bg-white-100">
+          <li>Personal</li>
+          <li>Professional</li>
+        </ul>
+        <div className='flex-col items-center justify-center w-[500px] mx-auto'>
+          <form className="py-2 flex justify-evenly">
+            <input className='bg-white-0' type="text" placeholder='Enter your Todo... '/>
+            <button>Add Todo</button>
+          </form>
+          <div className='w-full bg-white-0'>
+            <p>Take a walk in the park</p>
+            <span>Delete</span>
           </div>
-          <div>item</div>
-        </section>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
