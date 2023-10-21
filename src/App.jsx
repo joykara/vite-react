6b0b249119360706d28a3a412795c21dd4303e5b
joykara <joykarani1@gmail.com>
2023-10-21 13:22:35 +0300

    basic  layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {FaEdit, FaTrash} from 'react-icons/fa';
 
 function App() {
   const [todo, setTodo] = useState();
@@ -6,20 +7,24 @@ function App() {
   return (
     <div className="flex h-screen w-full bg-violet-100 bg-opacity-95">
       <div className="flex-col justify-center w-full mx-auto">
-        <h1 className='text-center font-serif p-5'>TODO LIST</h1>
-        <ul className="flex justify-evenly py-2 border-b bg-white-100">
-          <li>Personal</li>
+        <h1 className='text-center font-serif font-medium text-3xl p-5'>TODO LIST</h1>
+        <ul className="flex justify-evenly py-2 border-b  bg-white">
+          <li className='border-r-2'>Personal</li>
           <li>Professional</li>
         </ul>
         <div className='flex-col items-center justify-center w-[500px] mx-auto'>
-          <form className="py-2 flex justify-evenly">
-            <input className='bg-white-0' type="text" placeholder='Enter your Todo... '/>
-            <button>Add Todo</button>
+          <form className="py-2 w-full flex justify-between my-4">
+            <input className='bg-white-0 w-96 p-2' type="text" placeholder='Enter your Todo... '/>
+            <button className='bg-violet-700 text-white p-2'>Add Todo</button>
           </form>
-          <div className='w-full bg-white-0'>
-            <p>Take a walk in the park</p>
-            <span>Delete</span>
-          </div>
+          <ul className='flex w-full mx-auto items-center bg-white'>
+            <li className='flex justify-between p-2 w-full border-b-2 border-violet-200'>
+              <p>Take a walk in the park</p>
+              <span className='flex gap-3 mr-2'>
+                <FaEdit color='green' /><FaTrash color='purple' />
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
